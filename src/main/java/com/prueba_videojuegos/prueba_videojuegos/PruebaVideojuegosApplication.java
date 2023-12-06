36173f3830f3244aae8a22dcd96f3af1d14538d9
Jacinto Escobar Quezada <eqjo190657@upemor.edu.mx>
2023-12-05 20:32:42 -0600

    Primer commit

---
+++ src/main/java/com/prueba_videojuegos/prueba_videojuegos/PruebaVideojuegosApplication.java
@@ -0,0 +1,13 @@
+package com.prueba_videojuegos.prueba_videojuegos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebaVideojuegosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebaVideojuegosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/prueba_videojuegos/prueba_videojuegos/PruebaVideojuegosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prueba_videojuegos.prueba_videojuegos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebaVideojuegosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
